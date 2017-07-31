374f2ea8693e43d4637e7f8364a79dceef8cc8d5
Stian Lind Petlund <stianlp@gmail.com>
2017-07-31 18:09:49 +0200

    Added dependency in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='predict_client',
-    version='1.3.0',
+    version='1.3.1',
     description='Client used to send grcp requests to a tfserving model',
     url='https://github.com/epigramai/tfserving_predict_client',
     author='Stian Lind Petlund',
     author_email='stian@epigram.ai',
-    packages=find_packages('.')
+    packages=find_packages('.'),
+    install_requires=['grpcio>=0.15.0']
 )
