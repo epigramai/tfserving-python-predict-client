2a6a07d6679c73f9d8c6b63c0dc994f5adefcf2c
Stian Lind Petlund <stianlp@gmail.com>
2017-07-31 13:56:50 +0200

    Initial commit predict client

---
+++ predict_client/client.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import grpc
+import logging
+
+from predict_client.predict_pb2 import PredictRequest
+from predict_client.prediction_service_pb2 import PredictionServiceStub
+
+logger = logging.getLogger(__name__)
+
+
+def predict(img,  model_name, model_version, host='localhost', port='9000', is_batch_shaped=True):
+    REQUEST_TIMEOUT = 10
+    host = host + ':' + port
+
+    tensor_shape = img.shape
+
+    logger.debug('Image shape: ' + str(tensor_shape))
+
+    if is_batch_shaped:
+        tensor_shape = (1,) + tensor_shape
+
+    if model_name == 'incv4' or model_name == 'res152':
+        features_tensor_proto = tf.contrib.util.make_tensor_proto(img, shape=tensor_shape)
+    else:
+        features_tensor_proto = tf.contrib.util.make_tensor_proto(img,
+                                                                  dtype=tf.float32, shape=tensor_shape)
+
+    # Create gRPC client and request
+    channel = grpc.insecure_channel(host)
+    stub = PredictionServiceStub(channel)
+    request = PredictRequest()
+
+    request.model_spec.name = model_name
+
+    # request.model_spec.signature_name = 'serving_default'
+    if model_version > 0:
+        request.model_spec.version.value = model_version
+
+    request.inputs['inputs'].CopyFrom(features_tensor_proto)
+
+    # Send request
+    result = stub.Predict(request, timeout=REQUEST_TIMEOUT)
+
+    return list(result.outputs['scores'].float_val)
+
+
+if __name__ == '__main__':
+    import cv2
+    img = cv2.imread('../data/correct-2-2.png', cv2.IMREAD_GRAYSCALE)
+    # predict takes an image with shape (height, width, depth)
+    img = img.reshape((img.shape + (1,)))
+    pred = predict(img, 'mnist', 1)
+    print(pred)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='predict_client',
+    version='1.0.0',
+    description='Client used to send grcp requests to a tfserving model',
+    url='https://github.com/epigramai/tfserving_predict_client',
+    author='Stian Lind Petlund',
+    author_email='stian@epigram.ai',
+    packages=find_packages('.')
+)
