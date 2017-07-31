80388e738ec47ea87cbf065853e6e89fe987dafa
Stian Lind Petlund <stianlp@gmail.com>
2017-07-31 15:29:07 +0200

    Version change after some renaming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='predict_client',
-    version='1.0.0',
+    version='1.1.0',
     description='Client used to send grcp requests to a tfserving model',
     url='https://github.com/epigramai/tfserving_predict_client',
     author='Stian Lind Petlund',
