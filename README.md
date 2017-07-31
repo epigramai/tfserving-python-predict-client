75f230d647c3de799e69d2e9743be9e233c651a2
Stian Lind Petlund <stianlp@gmail.com>
2017-07-31 15:15:43 +0200

    updated readme

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ The predict client is meant to be used with a model served with tfserving. Becau
 be requested by just sending a normal HTTP request. The predict_client package is a grcp client that can request the service.
 Feel free to use this package to integrate your python apis with tfserving models.
 
+## How to use
+def predict(request_data, model_name, model_version, host='localhost', port='9000', is_batch_shaped=True, request_timeout=10):
+ - request_data: the data as a numpy array, in batches
+ - model_name: the name of the model we want to request
+ - model_version: the model version we want to request
+ - host: where the tfserving model is hosted
+ - port: port of the host
+ - is_batch_shaped: by default tfserving expects the images to come in batches, set this parameter to False and the client will add a wrapping dimension before sending the data to the tfserving model
+ - request_timeout: timeout sent to the grcp stub
+ 
 ## Examples
 The mnist example expects an mnist model to be served on localhost:9000. In order to run examples/mnist.py you need to install flask.
 Send a POST request to localhost:5000 with an mnist image, and you should get a response with predictions for mnist back.
--- predict_client/client.py
+++ predict_client/client.py
@@ -8,11 +8,10 @@ from predict_client.prediction_service_pb2 import PredictionServiceStub
 logger = logging.getLogger(__name__)
 
 
-def predict(img,  model_name, model_version, host='localhost', port='9000', is_batch_shaped=True):
-    REQUEST_TIMEOUT = 10
+def predict(request_data,  model_name, model_version, host='localhost', port='9000', is_batch_shaped=True, request_timeout=10):
     host = host + ':' + port
 
-    tensor_shape = img.shape
+    tensor_shape = request_data.shape
 
     logger.debug('Image shape: ' + str(tensor_shape))
 
@@ -20,9 +19,9 @@ def predict(img,  model_name, model_version, host='localhost', port='9000', is_b
         tensor_shape = (1,) + tensor_shape
 
     if model_name == 'incv4' or model_name == 'res152':
-        features_tensor_proto = tf.contrib.util.make_tensor_proto(img, shape=tensor_shape)
+        features_tensor_proto = tf.contrib.util.make_tensor_proto(request_data, shape=tensor_shape)
     else:
-        features_tensor_proto = tf.contrib.util.make_tensor_proto(img,
+        features_tensor_proto = tf.contrib.util.make_tensor_proto(request_data,
                                                                   dtype=tf.float32, shape=tensor_shape)
 
     # Create gRPC client and request
@@ -39,7 +38,7 @@ def predict(img,  model_name, model_version, host='localhost', port='9000', is_b
     request.inputs['inputs'].CopyFrom(features_tensor_proto)
 
     # Send request
-    result = stub.Predict(request, timeout=REQUEST_TIMEOUT)
+    result = stub.Predict(request, timeout=request_timeout)
 
     return list(result.outputs['scores'].float_val)
 
