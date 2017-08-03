6379c008253838dd3c4f00617eda7a4c97f75e51
Stian Lind Petlund <stianlp@gmail.com>
2017-08-03 14:37:19 +0200

    Forgot to update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='predict_client',
-    version='1.4.0',
+    version='1.4.2',
     description='Client used to send grcp requests to a tfserving model',
     url='https://github.com/epigramai/tfserving_predict_client',
     author='Stian Lind Petlund',
