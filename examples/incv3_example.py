6240cffdfc25a2562a3113ba94a10c9048fc6345
Stian Lind Petlund <stianlp@gmail.com>
2017-10-11 20:48:19 +0200

    temp commit

--- examples/incv3_example.py
+++ examples/incv3_example.py
@@ -2,9 +2,9 @@
 import logging
 import cv2
 import numpy as np
-
 from predict_client.prod_client import PredictClient
-
+import time
+import os
 
 # Logger initialization
 # This must happen before any calls to debug(), info(), etc.
@@ -15,15 +15,28 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 host = 'localhost:9000'
-model_name = 'incv4'
-model_version = 1
+model_name = 'incv3'
+model_version = 2
 
-incv4_client = PredictClient(host, model_name, model_version)
+incv3_client = PredictClient(host, model_name, model_version)
 
-img = cv2.imread('test_image.jpg')
+# for f in os.listdir('../test_data/res'):
+#     if not f.endswith('.jpg'):
+#         continue
+
+img = cv2.imread(
+    os.path.join('/Users/slp/Development/tfserving_predict_client/test_data/car_images/Image1857451_5.jpg'))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (299, 299))
+# img = np.expand_dims(img, axis=0)
+
+t = time.time()
 
 ''' Return value will be None if model not running on host '''
-prediction = incv4_client.predict(img)
+prediction = incv3_client.predict(np.array([img]))
+
+logger.info('Request time: ' + str(time.time() - t))
 
 logger.info('Features of length: ' + str(len(prediction)))
 logger.info('First 10 features: ' + str(prediction[:10]))
+# time.sleep(20)
--- predict_client/mock_client.py
+++ predict_client/mock_client.py
@@ -1,54 +1,53 @@
 import tensorflow as tf
-from tensorflow.python.saved_model import signature_constants
+import grpc
 import logging
+
+from grpc import RpcError
+from predict_client.predict_pb2 import PredictRequest
+from predict_client.prediction_service_pb2 import PredictionServiceStub
 from predict_client.abstract_client import AbstractPredictClient
-from tfwrapper import twimage
 
 logger = logging.getLogger(__name__)
 
 
-class MockPredictClient(AbstractPredictClient):
+class LocalFallbackClient(AbstractPredictClient):
 
-    def __init__(self, host, model_name, model_version, model_path=None):
-        super().__init__(host, model_name, model_version)
-
-        if not model_path:
-            logging.error('MockPredictClient needs a .pd file to load!')
-            exit(1)
-
-        sess = tf.Session()
-        tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], model_path)
-
-        print(tf.saved_model.main_op.main_op())
-        # with tf.gfile.FastGFile(model_path, 'rb') as f:
-        #     print(model_path, f)
-        #     graph_def = tf.GraphDef()
-        #     graph_def.ParseFromString(f.read())
-        #     tf.import_graph_def(graph_def, name='')
-
-        img = twimage.imread('request.png')
-
-        #print(img)
-
-        # x = [n.name for n in tf.get_default_graph().as_graph_def().node]
-        # for e in x:
-        #     print(e)
-
-        predict_op = sess.graph.get_tensor_by_name(signature_constants.CLASSIFY_OUTPUT_SCORES)
-        sess.run(predict_op, feed_dict={signature_constants.CLASSIFY_INPUTS: img})
+    def __init__(self, host, model_name, model_version, num_scores=0):
+        super().__init__(host, model_name, model_version, num_scores)
 
     def predict(self, request_data, request_timeout=10):
 
-        logger.info('Sending request to in memory model')
+        logger.info('Sending request to tfserving model')
         logger.info('Model name: ' + str(self.model_name))
         logger.info('Model version: ' + str(self.model_version))
         logger.info('Host: ' + str(self.host))
 
-        with tf.Session() as sess:
-            print(signature_constants.CLASSIFY_OUTPUT_SCORES, signature_constants.CLASSIFY_INPUTS)
+        tensor_shape = request_data.shape
 
-            x = [n.name for n in tf.get_default_graph().as_graph_def().node]
-            for e in x:
-                print(e)
-            predict_op = sess.graph.get_tensor_by_name(signature_constants.CLASSIFY_OUTPUT_SCORES + ':0')
-            sess.run(predict_op, feed_dict={signature_constants.CLASSIFY_INPUTS + ':0': request_data})
+        if self.model_name == 'incv4' or self.model_name == 'res152':
+            features_tensor_proto = tf.contrib.util.make_tensor_proto(request_data, shape=tensor_shape)
+        else:
+            features_tensor_proto = tf.contrib.util.make_tensor_proto(request_data,
+                                                                      dtype=tf.float32, shape=tensor_shape)
+
+        # Create gRPC client and request
+        channel = grpc.insecure_channel(self.host)
+        stub = PredictionServiceStub(channel)
+        request = PredictRequest()
+
+        request.model_spec.name = self.model_name
+
+        if self.model_version > 0:
+            request.model_spec.version.value = self.model_version
+
+        request.inputs['inputs'].CopyFrom(features_tensor_proto)
+
+        try:
+            result = stub.Predict(request, timeout=request_timeout)
+            logger.debug('Predicted scores with len: ' + str(len(list(result.outputs['scores'].float_val))))
+            return list(result.outputs['scores'].float_val)
+        except RpcError as e:
+            logger.warning(e)
+            logger.warning('Prediction failed. Mock client will return empty prediction of length: '
+                           + str(self.num_scores))
+            return [0] * self.num_scores
