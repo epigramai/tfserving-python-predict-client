dd89ea6686db79dc9302c5fa64c61d7d908cd23b
Stian Lind Petlund <stianlp@gmail.com>
2018-05-29 11:11:44 +0200

    Make sure grcpio version is 1.11.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='predict-client',
-    version='1.7.1',
+    version='1.7.2',
     description='Client used to send grcp requests to a tfserving model',
     url='https://github.com/epigramai/tfserving_predict_client',
     author='Stian Lind Petlund',
     author_email='stian@epigram.ai',
     packages=find_packages('.'),
-    install_requires=['grpcio>=0.15.0', 'numpy==1.13.1']
+    install_requires=['grpcio==1.11.0', 'numpy==1.13.1']
 )
