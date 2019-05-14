26a099a13a287a865ea80fa630a828371b770a7d
johanjk <johan@epigram.ai>
2019-05-14 09:37:04 +0200

    Propperly coses grpc channel after use
    Added minimal documentation
    updated grpcio version

--- predict_client/prod_client.py
+++ predict_client/prod_client.py
@@ -1,5 +1,7 @@
 import logging
 import time
+from typing import List, Dict, Any
+
 import grpc
 from grpc import RpcError
 from predict_client.pbs.prediction_service_pb2 import PredictionServiceStub
@@ -8,7 +10,7 @@ from predict_client.util import predict_response_to_dict, make_tensor_proto
 
 
 class ProdClient:
-    def __init__(self, host, model_name, model_version):
+    def __init__(self, host: str, model_name: str, model_version: int):
 
         self.logger = logging.getLogger(self.__class__.__name__)
 
@@ -16,8 +18,30 @@ class ProdClient:
         self.model_name = model_name
         self.model_version = model_version
 
-    def predict(self, request_data, request_timeout=10):
+    def predict(self, request_data: List[Dict[str, Any]], request_timeout: int = 10):
+        """ Get a model prediction on request data
 
+        Args:
+            request_data: List of input graph nodes in the model, containing the following required fields:
+                'data': The data to get predictions on
+                'in_tensor_dtype': The datatype of the input, legal types are the keys amongst "dtype_to_number", defined in util.py
+                'in_tensor_name': The name of the models input graph node, often 'inputs'
+
+            request_timeout: timeout in seconds
+
+        Returns:
+            Empty dict on error, otherwise a dict with keys set as output graph node names and values set to their predicted value
+
+        Example:
+            >>> prod_client = ProdClient(host='localhost:9000', model_name='mnist', model_version=1)
+            >>> prod_client.predict(
+            ...     request_data=[{
+            ...         'data': np.asarray([image]),
+            ...         'in_tensor_dtype': 'DT_UINT8',
+            ...         'in_tensor_name': 'inputs',
+            ...     }]
+            ... )
+        """
         self.logger.info('Sending request to tfserving model')
         self.logger.info('Host: {}'.format(self.host))
         self.logger.info('Model name: {}'.format(self.model_name))
@@ -25,44 +49,45 @@ class ProdClient:
 
         # Create gRPC client and request
         t = time.time()
-        channel = grpc.insecure_channel(self.host)
-        self.logger.debug('Establishing insecure channel took: {}'.format(time.time() - t))
+        with grpc.insecure_channel(self.host) as channel:
 
-        t = time.time()
-        stub = PredictionServiceStub(channel)
-        self.logger.debug('Creating stub took: {}'.format(time.time() - t))
+            self.logger.debug('Establishing insecure channel took: {}'.format(time.time() - t))
 
-        t = time.time()
-        request = PredictRequest()
-        self.logger.debug('Creating request object took: {}'.format(time.time() - t))
-
-        request.model_spec.name = self.model_name
-
-        if self.model_version > 0:
-            request.model_spec.version.value = self.model_version
-
-        t = time.time()
-        for d in request_data:
-            tensor_proto = make_tensor_proto(d['data'], d['in_tensor_dtype'])
-            request.inputs[d['in_tensor_name']].CopyFrom(tensor_proto)
-
-        self.logger.debug('Making tensor protos took: {}'.format(time.time() - t))
-
-        try:
             t = time.time()
-            predict_response = stub.Predict(request, timeout=request_timeout)
+            stub = PredictionServiceStub(channel)
+            self.logger.debug('Creating stub took: {}'.format(time.time() - t))
 
-            self.logger.debug('Actual request took: {} seconds'.format(time.time() - t))
+            t = time.time()
+            request = PredictRequest()
+            self.logger.debug('Creating request object took: {}'.format(time.time() - t))
 
-            predict_response_dict = predict_response_to_dict(predict_response)
+            request.model_spec.name = self.model_name
 
-            keys = [k for k in predict_response_dict]
-            self.logger.info('Got predict_response with keys: {}'.format(keys))
+            if self.model_version > 0:
+                request.model_spec.version.value = self.model_version
 
-            return predict_response_dict
+            t = time.time()
+            for d in request_data:
+                tensor_proto = make_tensor_proto(d['data'], d['in_tensor_dtype'])
+                request.inputs[d['in_tensor_name']].CopyFrom(tensor_proto)
 
-        except RpcError as e:
-            self.logger.error(e)
-            self.logger.error('Prediction failed!')
+            self.logger.debug('Making tensor protos took: {}'.format(time.time() - t))
 
-        return {}
+            try:
+                t = time.time()
+                predict_response = stub.Predict(request, timeout=request_timeout)
+
+                self.logger.debug('Actual request took: {} seconds'.format(time.time() - t))
+
+                predict_response_dict = predict_response_to_dict(predict_response)
+
+                keys = [k for k in predict_response_dict]
+                self.logger.info('Got predict_response with keys: {}'.format(keys))
+
+                return predict_response_dict
+
+            except RpcError as e:
+                self.logger.error(e)
+                self.logger.error('Prediction failed!')
+
+            return {}
